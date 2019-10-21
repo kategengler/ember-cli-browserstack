be34d7dd4259d54fd9cf4840ee38a99e8d0e67b0
Katie Gengler <katie@kmg.io>
2019-10-20 21:27:21 -0400

    Add some tests

---
+++ node-tests/commands/browserstack-browsers-test.js
@@ -0,0 +1,19 @@
+const execa = require('execa');
+const { assert } = require('chai');
+
+describe('ember browserstack:browsers', function() {
+  this.timeout(5000);
+  it('lists all browsers', async function() {
+    let { stdout } = await execa('ember', ['browserstack:browsers']);
+    let browsers = JSON.parse(stdout);
+    assert.ok(browsers.length);
+    assert.deepEqual(Object.keys(browsers[0]), [
+      'os',
+      'os_version',
+      'browser',
+      'device',
+      'browser_version',
+      'real_mobile'
+    ]);
+  });
+});
