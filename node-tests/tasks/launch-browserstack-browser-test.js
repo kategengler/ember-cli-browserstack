bd68abb3313c44bd3179d3594ec6525b45bcc215
Katie Gengler <katie@kmg.io>
2021-08-18 20:17:01 -0400

    Upgrade yargs

--- node-tests/tasks/launch-browserstack-browser-test.js
+++ node-tests/tasks/launch-browserstack-browser-test.js
@@ -11,17 +11,14 @@ describe('launch-browserstack-browser (launcher)', function () {
     assert.include(stdout, '--version');
     assert.include(stdout, '--os');
     assert.include(stdout, '--os_version, --osv');
-    assert.include(stdout, '--browser, -b');
+    assert.include(stdout, '-b, --browser');
     assert.include(stdout, '--browser_version, --bv');
-    assert.include(stdout, '--device, -d');
+    assert.include(stdout, '-d, --device');
     assert.include(stdout, '--real_mobile, --rm');
-    assert.include(stdout, '--url, -u');
-    assert.include(stdout, '--timeout, -t');
-    assert.include(
-      stdout,
-      'Timeout (min: 60, max: 1800)           [default: 300]'
-    );
-    assert.include(stdout, '--name, -n');
+    assert.include(stdout, '-u, --url');
+    assert.include(stdout, '-t, --timeout');
+    assert.include(stdout, 'Timeout (min: 60, max: 1800)       [default: 300]');
+    assert.include(stdout, '-n, --name');
     assert.include(stdout, '--build');
     assert.include(stdout, '--help');
   });
